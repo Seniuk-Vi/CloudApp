191ebcdd19f1ad52903d377aaeb3c040af5ea948
seniuk <vitaliiseniuk04@gmail.com>
2023-02-22 09:31:20 +0200

    Initial commit

---
+++ src/test/java/com/pet/cloudapp/CloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.cloudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
